72d28895115c3f0d92e0547d3abb531fd4b1af8b
Renato Gamper <gamper.renato@gmail.com>
2022-01-12 20:24:57 +0100

    increase header height

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -4,7 +4,13 @@ import { Navbar, Nav, Container } from "react-bootstrap";
 const Header = () => {
   return (
     <header>
-      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+      <Navbar
+        className="py-4"
+        bg="dark"
+        variant="dark"
+        expand="lg"
+        collapseOnSelect
+      >
         <Container>
           <Navbar.Brand href="/">eShop</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
