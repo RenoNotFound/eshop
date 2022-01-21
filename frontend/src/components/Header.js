aab47bdd7c12f411d8362e2e9d0a7f64e9f0d1d1
Renato Gamper <gamper.renato@gmail.com>
2022-01-21 14:51:09 +0100

    display username in navbar if user logged in

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,8 +1,16 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
-import { Navbar, Nav, Container } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import { Navbar, Nav, NavDropdown, Container } from "react-bootstrap";
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
+  const logoutHandler = () => {};
+
   return (
     <header>
       <Navbar
@@ -22,9 +30,20 @@ const Header = () => {
               <Link to="/cart" className="nav-link">
                 <i className="fas fa-shopping-cart"></i> Cart
               </Link>
-              <Link to="/login" className="nav-link">
-                <i className="fas fa-user"></i> Sign in
-              </Link>
+              {userInfo ? (
+                <NavDropdown title={userInfo.name} id="username">
+                  <LinkContainer to="/profile">
+                    <NavDropdown.Item>Profile</NavDropdown.Item>
+                  </LinkContainer>
+                  <NavDropdown.Item onClick={logoutHandler}>
+                    Logout
+                  </NavDropdown.Item>
+                </NavDropdown>
+              ) : (
+                <Link to="/login" className="nav-link">
+                  <i className="fas fa-user"></i> Sign in
+                </Link>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
