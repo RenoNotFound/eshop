95cb8b3a00265406511a2164586556a0918048fd
Renato Gamper <gamper.renato@gmail.com>
2022-01-21 14:35:56 +0100

    create login screen and display a form to log in

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
+import LoginScreen from "./screens/LoginScreen";
 
 const App = () => {
   return (
@@ -19,6 +20,7 @@ const App = () => {
             <Route path="/" element={<HomeScreen />} />
             <Route path="/product/:id" element={<ProductScreen />} />
             <Route path="/cart" element={<CartScreen />} />
+            <Route path="/login" element={<LoginScreen />} />
           </Routes>
         </Container>
       </main>
