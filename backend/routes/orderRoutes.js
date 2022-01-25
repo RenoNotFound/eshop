896a97237311bf1ad2aab019e9f8a6b7a9acf00f
Renato Gamper <gamper.renato@gmail.com>
2022-01-25 17:39:30 +0100

    fix import route

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { addOrderItems } from "../controllers/productController.js";
+import { addOrderItems } from "../controllers/orderController.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
