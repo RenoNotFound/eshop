f0c2b92ab4861d01ca141cf8584c45f7898fff2c
Renato Gamper <gamper.renato@gmail.com>
2022-01-26 15:01:21 +0100

    fix typos

--- frontend/src/reducers/orderReducer.js
+++ frontend/src/reducers/orderReducer.js
@@ -21,7 +21,7 @@ export const orderCreateReducer = (state = {}, action) => {
 };
 
 export const orderDetailsReducer = (
-  state = { loading: true, orderItems: [], shippingAddress: {} },
+  state = { orderItems: [], shippingAddress: {} },
   action
 ) => {
   switch (action.type) {
