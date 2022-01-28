bd825e6ad8bf768b07a8850bf8969e1ef99713b2
Renato Gamper <gamper.renato@gmail.com>
2022-01-28 15:44:11 +0100

    add disabled props

--- frontend/src/components/CheckoutSteps.js
+++ frontend/src/components/CheckoutSteps.js
@@ -3,15 +3,19 @@ import { Nav } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 import Done from "./Done";
 
-const CheckoutSteps = ({ step1, step2, step3, step4 }) => {
+const CheckoutSteps = ({ step1, step2, step3, step4, disabled }) => {
   return (
     <Nav className="justify-content-center mb-4">
       <Nav.Item className="d-flex flex-row align-items-center">
         {step1 ? (
           <>
-            <LinkContainer to="/login" className="px-2">
-              <Nav.Link>Sign In</Nav.Link>
-            </LinkContainer>
+            {disabled ? (
+              <Nav.Link disabled>Sign In</Nav.Link>
+            ) : (
+              <LinkContainer to="/login" className="px-2">
+                <Nav.Link>Sign In</Nav.Link>
+              </LinkContainer>
+            )}
             <Done isDone={step1} />
           </>
         ) : (
@@ -22,9 +26,13 @@ const CheckoutSteps = ({ step1, step2, step3, step4 }) => {
       <Nav.Item className="d-flex flex-row align-items-center">
         {step2 ? (
           <>
-            <LinkContainer to="/shipping" className="px-2">
-              <Nav.Link>Shipping</Nav.Link>
-            </LinkContainer>
+            {disabled ? (
+              <Nav.Link disabled>Shipping</Nav.Link>
+            ) : (
+              <LinkContainer to="/shipping" className="px-2">
+                <Nav.Link>Shipping</Nav.Link>
+              </LinkContainer>
+            )}
             <Done isDone={step2} />
           </>
         ) : (
@@ -35,9 +43,13 @@ const CheckoutSteps = ({ step1, step2, step3, step4 }) => {
       <Nav.Item className="d-flex flex-row align-items-center">
         {step3 ? (
           <>
-            <LinkContainer to="/payment" className="px-2">
-              <Nav.Link>Payment</Nav.Link>
-            </LinkContainer>
+            {disabled ? (
+              <Nav.Link disabled>Payment</Nav.Link>
+            ) : (
+              <LinkContainer to="/payment" className="px-2">
+                <Nav.Link>Payment</Nav.Link>
+              </LinkContainer>
+            )}
             <Done isDone={step3} />
           </>
         ) : (
@@ -48,9 +60,13 @@ const CheckoutSteps = ({ step1, step2, step3, step4 }) => {
       <Nav.Item className="d-flex flex-row align-items-center">
         {step4 ? (
           <>
-            <LinkContainer to="/placeorder" className="px-2">
-              <Nav.Link>Place Order</Nav.Link>
-            </LinkContainer>
+            {disabled ? (
+              <Nav.Link disabled>Place Order</Nav.Link>
+            ) : (
+              <LinkContainer to="/placeorder" className="px-2">
+                <Nav.Link>Place Order</Nav.Link>
+              </LinkContainer>
+            )}
             <Done isDone={step4} />
           </>
         ) : (
