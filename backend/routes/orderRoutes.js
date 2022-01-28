ff0c356ec11b28080911b6745ee9afba2ac567ee
Renato Gamper <gamper.renato@gmail.com>
2022-01-28 11:47:42 +0100

    create route and function to get logged in users orders in controller

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -3,11 +3,13 @@ import {
   addOrderItems,
   getOrderById,
   updateOrderToPaid,
+  getMyOrders,
 } from "../controllers/orderController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
+router.get("/myorders", protect, getMyOrders);
 router.post("/", protect, addOrderItems);
 router.get("/:id", protect, getOrderById);
 router.put("/:id/pay", protect, updateOrderToPaid);
