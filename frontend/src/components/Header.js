7a87c005d50da1563686063ef1481df0c01ecb44
Renato Gamper <gamper.renato@gmail.com>
2022-02-15 12:38:52 +0100

    create admin dropdown in header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -53,6 +53,19 @@ const Header = () => {
                   <i className="fas fa-user"></i> Sign in
                 </Link>
               )}
+              {userInfo && userInfo.isAdmin && (
+                <NavDropdown title="Admin" id="adminmenu">
+                  <LinkContainer to="/admin/userlist">
+                    <NavDropdown.Item>Users</NavDropdown.Item>
+                  </LinkContainer>
+                  <LinkContainer to="/admin/productlist">
+                    <NavDropdown.Item>Products</NavDropdown.Item>
+                  </LinkContainer>
+                  <LinkContainer to="/admin/orderlist">
+                    <NavDropdown.Item>Orders</NavDropdown.Item>
+                  </LinkContainer>
+                </NavDropdown>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
