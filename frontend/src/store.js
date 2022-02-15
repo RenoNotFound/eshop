faedf9c2fe34161b43aef1ea2ee901a84f954cee
Renato Gamper <gamper.renato@gmail.com>
2022-02-15 16:04:59 +0100

    create route, action and reducer to delete user from userlist

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -11,6 +11,7 @@ import {
   userDetailsReducer,
   userUpdateProfileReducer,
   userListReducer,
+  userDeleteReducer,
 } from "./reducers/userReducer";
 import {
   orderCreateReducer,
@@ -28,6 +29,7 @@ const reducer = combineReducers({
   userDetails: userDetailsReducer,
   userUpdateProfile: userUpdateProfileReducer,
   userList: userListReducer,
+  userDelete: userDeleteReducer,
   orderCreate: orderCreateReducer,
   orderDetails: orderDetailsReducer,
   orderPay: orderPayReducer,
